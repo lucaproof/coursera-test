1bcc0c50cbee116c0ecb9fd810a207beaa7c93e8
Luca Montanari <luchino1194@gmail.com>
2022-12-21 16:34:43 +0100

    Signed-off-by: Luca Montanari <luchino1194@gmail.com>

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -52,7 +52,7 @@ WARNING!!! WARNING!!!
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = names[i].charAt().toLowerCase()
+  var firstLetter = names[i].charAt().toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
@@ -60,9 +60,9 @@ WARNING!!! WARNING!!!
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
         if (firstLetter==="j") {
-    byeSpeaker.speak(names[i])
+    byeSpeaker.speak(names[i]);
       } else {
-    helloSpeaker.speak(names[i])
+    helloSpeaker.speak(names[i]);
   }
 }
 
