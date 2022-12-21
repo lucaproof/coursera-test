9a7b6298e9f16ce5e95223cad9fe8324f26ca239
Luca Montanari <luchino1194@gmail.com>
2022-12-21 16:21:07 +0100

    Signed-off-by: Luca Montanari <luchino1194@gmail.com>

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -52,7 +52,7 @@ WARNING!!! WARNING!!!
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = names[i].charAt().toLowerCase()
+  var firstLetter = names.charAt().toLowerCase()
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
